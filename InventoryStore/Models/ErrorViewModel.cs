1495298fd66b6690dc9203a36b6a230876a834ca
Daniel Flores <dflores2t@gmail.com>
2023-01-22 14:37:44 -0600

    Start nem InventoryStore Project

---
+++ InventoryStore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
